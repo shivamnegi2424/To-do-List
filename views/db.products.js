22514ff5a86c249ef369b78108045ee449edf349
Shivam Negi <shivamnegi.2424@gmail.com>
2023-07-03 11:50:56 +0530

    to-do

---
+++ views/db.products.js
@@ -0,0 +1,18 @@
+db.products.insert({
+    _id:3,
+    name:"Rubber",
+    price:1.30,
+    stock:43,
+    reviews :[
+        {
+            authorName:"Rebecca",
+            rating:5,
+            review:"GOAT rubber!"
+        },
+        {
+            authorName:"John",
+            rating:5,
+            review:"AWesome rubber!"
+        }
+    ]
+})
